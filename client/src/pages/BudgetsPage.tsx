38cc0aff6ab8b33b7faf15c245f32f5995a3fb25
Ajene <ajenec@gmail.com>
2025-10-27 02:56:05 -0400

    creating branch

--- client/src/pages/BudgetsPage.tsx
+++ client/src/pages/BudgetsPage.tsx
@@ -1,14 +1,20 @@
 import React from "react";
 import BudgetForm from "../components/budgetComp/BudgetForm";
 import BudgetList from "../components/budgetComp/BudgetList";
+import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
 
 const BudgetsPage: React.FC = () => {
   return (
-    <div>
-      <h1>Budgets Page</h1>
-      <BudgetForm budget={0} />
-      <BudgetList budgets={[]} />
-    </div>
+    <>
+      <NavBar />
+      <div>
+        <h1>Budgets Page</h1>
+        <BudgetForm budget={0} />
+        <BudgetList budgets={[]} />
+      </div>
+      <Footer />
+    </>
   );
 };
 
